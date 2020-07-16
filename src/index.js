d6e9a1612df097f178262effc8f3a525878f4aa8
Abhishek Gangadhar <abhishekgangadhar95@gmail.com>
2020-07-17 02:18:09 +0530

    Making squares variable immutable

--- src/index.js
+++ src/index.js
@@ -82,12 +82,12 @@ class Game extends React.Component {
     handleClick(i) {
         const history = this.state.history.slice(0, this.state.stepNumber + 1);
         const current = history[history.length - 1];
-        const squares = current.squares;
+        const squares = current.squares.slice();
         if (calculateWinner(squares) || squares[i]) { return }
         squares[i] = this.state.xIsNext ? 'X': 'O';
         this.setState({
             history: history.concat([{
-                squares: squares,
+                squares: squares
             }]),
             stepNumber: history.length,
             xIsNext: !this.state.xIsNext
@@ -98,7 +98,7 @@ class Game extends React.Component {
         console.log(step)
         this.setState({
             stepNumber: step,
-            xIsNext: (step % 2) === 0,
+            xIsNext: (step % 2) === 0
         })
     }
 
@@ -130,7 +130,7 @@ class Game extends React.Component {
                 <div className="game-board">
                     <Board
                         squares={current.squares}
-                        onClick={(i) => this.handleClick(i)}
+                        onClick={i => this.handleClick(i)}
                     />
                 </div>
                 <div className="game-info">
