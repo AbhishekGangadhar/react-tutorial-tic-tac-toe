885376983ebce77f2b9f3c4d18f657000e3ffac8
Abhishek Gangadhar <abhishekgangadhar95@gmail.com>
2020-07-17 00:18:35 +0530

    Updated Square class to show "X" on click of the square

--- src/index.js
+++ src/index.js
@@ -15,8 +15,11 @@ class Square extends React.Component {
 
     render() {
         return (
-            <button className="square" onClick={() => {alert("Click")}}>
-                {this.props.value}
+            <button
+                className="square"
+                onClick={() => {this.setState({value: "X"})}}
+            >
+                {this.state.value}
             </button>
         );
     }
